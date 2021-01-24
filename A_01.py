018965f62486e857a604eb43cc80a766dd613d31
Adil <mr.adil777@gmail.com>
2021-01-25 00:30:47 +0600

    feat(a_01.py): add task A
    	new file:   .gitignore
    	modified:   A_01.py
    	new file:   draft.ipynb
    	modified:   result/A_01.csv

---
+++ A_01.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import re
+
+# Task 1
+# Actually, I don't know how to deal with the names like НҮРДІБЕК БАЛЖАН, because they can be both M and F
+
+file_name = './data/State Grants 2020.xlsx' 
+df = pd.read_excel(file_name, sheet_name='General')
+
+M = r'ұлы|улы|уғли|угли|вич|ов|ев[^\w]'
+F = r'қызы|кызы|қизи|кизи|вна|вна|ова|ева[^\w]'
+
+def identifyGender(FIO):
+    FIO = FIO.lower()
+    if (re.search(F, FIO)):
+        return 'F'
+    if (re.search(M, FIO)):
+        return 'M'
+    return ''
+
+df['Gender'] = df.apply(lambda row: identifyGender(row.FIO), axis = 1)
+file_path = './result/A_01.csv'
+df.to_csv(file_path, columns=['FIO', 'Gender'])
+
+# bigender = df[df['Gender'] == '']
+# bigender.to_csv('bigender.csv')
+# print(bigender)
+# AS you can see, still 75 students have bigender names
+
