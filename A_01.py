5beb2baf873d9b57df7065e1fdde6acdc142af3a
Adil <mr.adil777@gmail.com>
2021-01-25 00:57:19 +0600

    refactor(a_01.py): merge sheets

--- A_01.py
+++ A_01.py
@@ -5,7 +5,7 @@ import re
 # Actually, I don't know how to deal with the names like НҮРДІБЕК БАЛЖАН, because they can be both M and F
 
 file_name = './data/State Grants 2020.xlsx' 
-df = pd.read_excel(file_name, sheet_name='General')
+df = pd.concat(pd.read_excel(file_name, sheet_name=[0,1,2,3,4]), ignore_index=True)[:-1]
 
 M = r'ұлы|улы|уғли|угли|вич|ов|ев[^\w]'
 F = r'қызы|кызы|қизи|кизи|вна|вна|ова|ева[^\w]'
@@ -25,5 +25,6 @@ df.to_csv(file_path, columns=['FIO', 'Gender'])
 # bigender = df[df['Gender'] == '']
 # bigender.to_csv('bigender.csv')
 # print(bigender)
-# AS you can see, still 75 students have bigender names
+# AS you can see, still 116 students have bigender names
+
 
