9d0d57ef3803362674b12fd0a9f5ca12b2af0f3a
Adil <mr.adil777@gmail.com>
2021-01-25 11:19:02 +0600

    feat(b_01.py): add task 2

---
+++ B_01.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import re
+import zipfile
+
+filename = './data/bbc-fulltext.zip'
+zip = zipfile.ZipFile(filename, 'r')
+files = zip.namelist()
+
+pattern = r'([A-Z]+\w*(?:\s*[A-Z]+\w*)+)'
+
+res = []
+
+for i in files:
+    s = str(zip.read(i))
+    names = re.findall(pattern,s)
+    res += list(filter(len, names))
+    
+s = pd.Series(res)
+s.drop_duplicates(keep='first',inplace=True)
+filepath = './result/B_01.csv'
+s.to_csv(filepath,index=None)
